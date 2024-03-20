f71865f3f5905553607b16adaf680458e88a921e
Mohammad Naim Dahee <dahee.naim@gmail.com>
2024-03-21 00:50:59 +0100

    Explicit Types

--- typescript/sandbox.js
+++ typescript/sandbox.js
@@ -1,16 +1,26 @@
-// Arrays
-// String arrays
-var names = ["ahmad", "mohammad"];
-// Number arrays
-var ages = [10, 15];
-// Mixed arrays
-var mixedArray = ["ahmad", 20, false];
-mixedArray.push(10, "mohammad", true, 30);
-console.log(mixedArray);
+// Explicit Types
+var firstName;
+var age;
+var isLoggedIn;
+firstName = "ahmad";
+age = 10;
+isLoggedIn = true;
+//Arrays
+var names = [];
+names.push("ahmad");
+// Union Types
+var union = [];
+union.push("ahmad", 10);
+var mixedValue;
+mixedValue = "mohammad";
+mixedValue = 10;
 // Objects
-var person = {
-    name: "ahmad",
-    age: 20,
-    isLogged: false,
+var person;
+person = { name: "ahmad", age: 10, isLogged: false };
+var user;
+user = {
+    firstName: "mohammad",
+    lastName: "ahmad",
+    dateOfBirth: new Date(),
 };
-console.log(person);
+console.log(user);
--- typescript/sandbox.ts
+++ typescript/sandbox.ts
@@ -1,19 +1,41 @@
-// Arrays
-// String arrays
-let names: string[] = ["ahmad", "mohammad"];
-// Number arrays
-let ages = [10, 15];
-// Mixed arrays
-let mixedArray: (string | number | boolean)[] = ["ahmad", 20, false];
-mixedArray.push(10, "mohammad", true, 30);
+// Explicit Types
+let firstName: string;
+let age: number;
+let isLoggedIn: boolean;
 
-console.log(mixedArray);
+firstName = "ahmad";
+age = 10;
+isLoggedIn = true;
+
+//Arrays
+let names: string[] = [];
+
+names.push("ahmad");
+// Union Types
+let union: (string | number)[] = [];
+union.push("ahmad", 10);
+
+let mixedValue: number | string;
+mixedValue = "mohammad";
+mixedValue = 10;
+
 // Objects
 
-const person = {
-  name: "ahmad",
-  age: 20,
-  isLogged: false,
+let person: object;
+
+person = { name: "ahmad", age: 10, isLogged: false };
+
+let user: {
+  firstName: string;
+  lastName: string;
+  dateOfBirth: Date;
+  isLogged?: boolean;
 };
 
-console.log(person);
+user = {
+  firstName: "mohammad",
+  lastName: "ahmad",
+  dateOfBirth: new Date(),
+};
+
+console.log(user);
