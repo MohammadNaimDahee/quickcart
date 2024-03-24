7e5062b7c163705b04be9b89cca0b9d2ec1e9c63
Mohammad Naim Dahee <dahee.naim@gmail.com>
2024-03-24 19:43:23 +0100

    Type Aliases

--- typescript/public/sandbox.js
+++ typescript/public/sandbox.js
@@ -1,2 +1,10 @@
 "use strict";
-console.log("Hello friend");
+// Type Aliases
+const sayHello = (id, name) => {
+    console.log(`${name} with ID ${id} says hello`);
+};
+sayHello(1, "Ahmad");
+const logUserDetails = (user) => {
+    console.log(`${user.id} - ${user.details}`);
+};
+logUserDetails({ id: 2, details: "User Details" });
--- typescript/src/sandbox.ts
+++ typescript/src/sandbox.ts
@@ -1,1 +1,15 @@
-console.log("Hello friend");
+// Type Aliases
+
+type StringOrNumber = string | number;
+
+const sayHello = (id: StringOrNumber, name: string) => {
+  console.log(`${name} with ID ${id} says hello`);
+};
+
+sayHello(1, "Ahmad");
+
+const logUserDetails = (user: { id: StringOrNumber; details: string }) => {
+  console.log(`${user.id} - ${user.details}`);
+};
+
+logUserDetails({ id: 2, details: "User Details" });
